b80cd0a623736620a20338943d20c671dee6a3a5
Yrran Jay Gaspado <yrranjayg@gmail.com>
2025-08-23 19:21:01 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoviesETicket.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
